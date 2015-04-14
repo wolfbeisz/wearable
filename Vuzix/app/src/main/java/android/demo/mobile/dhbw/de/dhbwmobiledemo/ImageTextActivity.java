216f0368bdf6a56e85c8a7493bf241097cb0d4b3
Suriwary <silkerohn@web.de>
2015-04-14 12:58:02 +0200

    merge commit

---
+++ Vuzix/app/src/main/java/android/demo/mobile/dhbw/de/dhbwmobiledemo/ImageTextActivity.java
@@ -0,0 +1,15 @@
+package android.demo.mobile.dhbw.de.dhbwmobiledemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Silke on 14.04.2015.
+ */
+public class ImageTextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagetext);
+    }
+}
--- Vuzix/app/src/main/java/android/demo/mobile/dhbw/de/dhbwmobiledemo/ImageTextNode.java
+++ Vuzix/app/src/main/java/android/demo/mobile/dhbw/de/dhbwmobiledemo/ImageTextNode.java
@@ -2,6 +2,7 @@ package android.demo.mobile.dhbw.de.dhbwmobiledemo;
 
 import android.content.Context;
 import android.graphics.Picture;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,6 +14,8 @@ public class ImageTextNode extends Node {
 
     private Picture picture;
     private String text;
+
+
 
     public ImageTextNode(int nodeId, String title, Picture logo, Picture picture, String text, String forwardText){
         super(nodeId, title, logo, forwardText);
