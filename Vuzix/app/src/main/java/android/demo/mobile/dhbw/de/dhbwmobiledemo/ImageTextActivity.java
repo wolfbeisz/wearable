b19e5b5e4d78f1d68dcafeff8179b5dd7611a318
Suriwary <silkerohn@web.de>
2015-04-14 13:50:46 +0200

    reference to node

--- Vuzix/app/src/main/java/android/demo/mobile/dhbw/de/dhbwmobiledemo/ImageTextActivity.java
+++ Vuzix/app/src/main/java/android/demo/mobile/dhbw/de/dhbwmobiledemo/ImageTextActivity.java
@@ -7,9 +7,17 @@ import android.os.Bundle;
  * Created by Silke on 14.04.2015.
  */
 public class ImageTextActivity extends Activity {
+
+    private static ImageTextNode node;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagetext);
+    }
+
+    public void setNode(ImageTextNode node){
+        this.node = node;
+
     }
 }
