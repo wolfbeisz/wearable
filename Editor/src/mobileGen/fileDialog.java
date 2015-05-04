8ef9d591812a98f805235237ff04c226f3ee8014
Richard Treu <mail@richardtreu.de>
2015-05-04 18:57:53 +0200

    JAR blödsinn...

--- Editor/src/mobileGen/fileDialog.java
+++ Editor/src/mobileGen/fileDialog.java
@@ -4,9 +4,16 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener; 
 import java.io.File; 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.swing.JFileChooser; 
+import javax.swing.JOptionPane;
 
 import org.apache.commons.io.FileUtils;
 
@@ -14,13 +21,12 @@ import com.sun.media.jai.rmi.DataBufferProxy;
 
 public class fileDialog { 
 	private static final String DB_PATH = "example-db.sqlite";//Dummy DB
-	
 	public String openDialog() { 
 		String ret = null;
 		final JFileChooser chooser = new JFileChooser("Choose file"); 
 		chooser.setDialogType(JFileChooser.OPEN_DIALOG); 
 		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES); 
-		final File file = new File("/Users/richardtreu/"); 
+		final File file = new File("/Users/"); 
 
 		chooser.setCurrentDirectory(file); 
 
@@ -78,47 +84,87 @@ public class fileDialog {
 		} 
 		fileChooser.setVisible(false);
 		try {
-			File demoDB = new File(DB_PATH);
-			FileUtils.copyFile(demoDB, fileP);
+			InputStream i = this.getClass().getClassLoader().getSystemResourceAsStream(DB_PATH);
+			OutputStream o = null;
+			o = new FileOutputStream(fileP);
+			int read = 0;
+			byte[] bytes = new byte[1000000];
+			while((read = i.read(bytes)) != -1){
+				o.write(bytes, 0, read);
+			}
+			System.out.println("Write done");
+			if(i !=null){
+				try{
+					i.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+			if(o !=null){
+				try{
+					o.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		return path;
 	}
 	public boolean saveDBFileOverride(String path) {
-		File fileP = new File(path);
 		try {
-			File demoDB = new File(DB_PATH);
-			FileUtils.copyFile(demoDB, fileP);
-			return true;
+			InputStream i = this.getClass().getClassLoader().getSystemResourceAsStream(DB_PATH);
+			OutputStream o = null;
+			o = new FileOutputStream(path);
+
+			int read = 0;
+			byte[] bytes = new byte[1000000];
+			while((read = i.read(bytes)) != -1){
+				o.write(bytes, 0, read);
+			}
+			System.out.println("Write done");
+			if(i !=null){
+				try{
+					i.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+			if(o !=null){
+				try{
+					o.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
 		}
-
+		return true;
 	}
-	 static public byte[] getByteArrayFromFile(String filePath){  
-	        byte[] ret=null;  
-	        FileInputStream fileInStream=null;  
-	        try{  
-	            File file=new File(filePath);  
-	            fileInStream=new FileInputStream(file);  
-	            long fileSize=file.length(); 
-	              
-	            if(fileSize>0 && fileSize<Integer.MAX_VALUE){  
-	                ret=new byte[(int)fileSize];  
-	                fileInStream.read(ret);  
-	            }  
-	        }catch(Exception e){  
-	            e.printStackTrace();  
-	        } finally {  
-	            try {  
-	                fileInStream.close();  
-	            } catch (Exception e) {  
-	            }  
-	        }  
-	        return ret;  
-	    }  
+	static public byte[] getByteArrayFromFile(String filePath){  
+		byte[] ret=null;  
+		FileInputStream fileInStream=null;  
+		try{  
+			File file=new File(filePath);  
+			fileInStream=new FileInputStream(file);  
+			long fileSize=file.length(); 
+
+			if(fileSize>0 && fileSize<Integer.MAX_VALUE){  
+				ret=new byte[(int)fileSize];  
+				fileInStream.read(ret);  
+			}  
+		}catch(Exception e){  
+			e.printStackTrace();  
+		} finally {  
+			try {  
+				fileInStream.close();  
+			} catch (Exception e) {  
+			}  
+		}  
+		return ret;  
+	}  
 
 } 
